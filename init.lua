c6bc5d8d817b35886a801b750a8cbcacc9fff2ec
rocketslime11 <cwellsny@nycap.rr.com>
2014-01-19 13:59:15 -0500

    fixed error in init file

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
 modpath=minetest.get_modpath("fireballs")
 
 dofile(modpath.."/fireball.lua")
-<<<<<<< HEAD
 dofile(modpath.."/light_ice.lua")
 dofile(modpath.."/thunder_sword.lua")
-=======
 dofile(modpath.."/light_ice.lua")
->>>>>>> e108048110a34156df2f500fb2d83ba932e4660f
